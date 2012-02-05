bfaa5dea9456ecfa1c7858967405757b2d824d30
lee <liwenjingabc@gmail.com>
2012-02-05 22:17:59 +0800

    change igno

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+# require File.expand_path("../../lib/rvm_cap", __FILE__)
+require 'bundler/capistrano'
+#set :rvm_ruby_string, '1.8.7'
+set :rvm_ruby_string, 'ree-1.8.7-2011.03'
+set :rvm_bin_path, "$HOME/.rvm/bin"
+# point to your server
+#set :host, '127.0.0.1'
+set :host,'106.187.47.151' #北美 
+role :db,host,:primary=>true
+#ssh_options[:keys] = ["#{ENV['HOME']}/sns.pem"]
+
+# if you have multiple servers, point these individually
+role :web, host
+
+# point to your github fork if you have one
+set :repository, "git://github.com/0612800232/team_tool.git"
+set :scm, :git
+
+set :application, 'teambox'
+#set :user, 'lee'
+set :user,'lee'
+set :runner, user
+set :repository_cache, 'git_cache'
+set :deploy_via, :remote_cache
+set :deploy_to, "/code/apps/#{application}"
+# set :copy_exclude, %w(test .git)
+set :use_sudo, false
+set :rvm_type, :user
